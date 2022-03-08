76a7e8012d8d6f17cf922fa2a5dfdc4a672a2106
AnvarKhan <45309516+AnvarKhan@users.noreply.github.com>
2022-03-08 14:20:21 +0500

    Login and Logout page with Authentication system

--- common/views.py
+++ common/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render
+
 from django.views.generic import TemplateView, CreateView
+
 from .models import Product
+
 from .forms import SignUpForms
+
 from django.urls import reverse_lazy
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+
 
 class HomeView(TemplateView):
     template_name = 'common/home.html'
+
+
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = 'common/dashboard.html'
+    login_url = reverse_lazy('home')
 
 
 class SignUpView(CreateView):
     form_class = SignUpForms
     success_url = reverse_lazy('home')
     template_name = 'common/register.html'
-
-
-class ProductCreateView(CreateView):
-    model = Product
-    fields = ('name', 'description', 'price')
-    template_name = 'product.html'
--- crm_main/urls.py
+++ crm_main/urls.py
@@ -15,10 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from common.views import HomeView, SignUpView
+from common.views import HomeView, SignUpView, DashboardView
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', HomeView.as_view(), name='home'),
     path('register/', SignUpView.as_view(), name='register'),
+
+    path('login/', auth_views.LoginView.as_view(
+    template_name='common/login.html'
+    ),
+    name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(
+    next_page='home'
+    ),
+         name='logout'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
 ]
